e5e067d9d1df0cdb61cdc55d0a53e6250bc87cd9
Patrick Aziken <onaziken@gmail.com>
2024-07-24 13:55:19 +0100

    feat: added unattended bypass for installs

--- stage-5-task/install.sh
+++ stage-5-task/install.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
 # Install necessary dependencies
-if ! sudo apt update && sudo apt -y upgrade; then
+if ! sudo DEBIAN_FRONTEND=noninteractive apt update && sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade; then
     echo "Could not update package list. Please try again."
     exit 1
 fi
 
-if ! sudo apt install -y net-tools; then
+if ! sudo DEBIAN_FRONTEND=noninteractive apt install -y net-tools; then
     echo "Could not install net-tools. Please try again."
     exit 1
 fi
 
-if ! sudo apt install -y nginx; then
+if ! sudo DEBIAN_FRONTEND=noninteractive apt install -y nginx; then
     echo "Could not install Nginx. Please try again."
     exit 1
 fi
 
-if ! sudo apt install -y docker.io && sudo systemctl start docker && sudo systemctl enable docker; then
+if ! sudo DEBIAN_FRONTEND=noninteractive apt install -y docker.io && sudo systemctl start docker && sudo systemctl enable docker; then
     echo "Could not install and enable Docker. Please try again."
     exit 1
 fi
-
-# if ! sudo groupadd docker; then
-#     # echo "docker group not created. Please try again."
-#     exit 1
-# fi
-
-# if ! sudo usermod -aG docker $USER ; then
-#     echo "The user was not added to the docker group. Please try again."
-#     exit 1
-# fi
 
 # Create a systemd service file for devopsfetch
 cat << EOF | sudo tee /etc/systemd/system/devopsfetch.service > /dev/null
@@ -65,9 +55,3 @@ if ! sudo systemctl start devopsfetch.service; then
     echo "Could not start devopsfetch service. Please try again."
     exit 1
 fi
-
-# Reload docker group...this should always be last
-# if ! newgrp docker ; then
-#     echo "docker group not reloaded. Please try again."
-#     exit 1
-# fi
